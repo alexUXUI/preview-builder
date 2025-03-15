9348e6353fffeda949ff6d13499eb1ca5828cc6b
Alex Bennett <alexandergraybennett@gmail.com>
2025-03-15 15:30:23 -0600

    deletes more code

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./uidsf/variables.css";
-import "./uidsf/reset.css";
-import "./uidsf/theme.css";
 
 const rootEl = document.getElementById("root");
 if (rootEl) {
--- src/uidsf/README.md
+++
@@ -1,7 +0,0 @@
-
-UI DSF (User Interface Design System Framework)
-================================================================
-
-
-## Icons
-Figma: https://www.figma.com/design/xg0CrVQgjtXosDaezgG4LJ/UI-Foundations?node-id=1082-14209&node-type=frame&m=dev
